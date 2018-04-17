fdd6b155442b13480dc59731c8b750513c84bb0d
Tom Wyant <wyant@cpan.org>
2018-04-17 13:14:02 -0400

    Version 0.000_001
    Set version to 0.000_001. Update Changes file for changes since .

--- lib/Perl/Critic/Policy/RegularExpressions/ProhibitNumericCharacterClasses.pm
+++ lib/Perl/Critic/Policy/RegularExpressions/ProhibitNumericCharacterClasses.pm
@@ -11,7 +11,7 @@ use Perl::Critic::Utils qw< :booleans :characters hashify :severities >;
 
 use base 'Perl::Critic::Policy';
 
-our $VERSION = '0.102';
+our $VERSION = '0.000_001';
 
 #-----------------------------------------------------------------------------
 
