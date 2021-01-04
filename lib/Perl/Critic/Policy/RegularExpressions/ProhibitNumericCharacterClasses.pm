1a6a8b53f7391ce3b599b5c398415c985baeb0d7
Tom Wyant <wyant@cpan.org>
2021-01-04 11:15:40 -0500

    Update copyright to 2021.

--- lib/Perl/Critic/Policy/RegularExpressions/ProhibitNumericCharacterClasses.pm
+++ lib/Perl/Critic/Policy/RegularExpressions/ProhibitNumericCharacterClasses.pm
@@ -803,7 +803,7 @@ Thomas R. Wyant, III F<wyant at cpan dot org>
 
 =head1 COPYRIGHT
 
-Copyright (C) 2018-2020 Thomas R. Wyant, III
+Copyright (C) 2018-2021 Thomas R. Wyant, III
 
 =head1 LICENSE
 
